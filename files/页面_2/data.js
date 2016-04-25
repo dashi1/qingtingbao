9412e4cbe816538c8901481f7fe67f3b2ed54717
dashi1 <1095846251@qq.com>
2016-04-25 18:15:44 +0800

    1
    1

---
+++ files/页面_2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x],y,_(z,A,B,C,D,E,F,_(),G,_(H,I,J,K,L,_(M,N,O,P),Q,null,R,K,S,K,T,U,V,null,W,X,Y,Z,ba,bb,bc,X),bd,_(),be,_(),bf,_(bg,[])),bh,_(),bi,_());}; 
+var b="url",c="页面_2.html",d="generationDate",e=new Date(1461578585346.7),f="isCanvasEnabled",g=false,h="variables",i="phone",j="sb",k="br",l="xm",m="jd",n="zd",o="zt",p="zy1",q="zy2",r="nr1",s="nr2",t="nr3",u="nr4",v="nr5",w="nr6",x="ys",y="page",z="packageId",A="dad20ba916b84397adbc37505e3f3443",B="type",C="Axure:Page",D="name",E="页面 2",F="notes",G="style",H="baseStyle",I="627587b6038d43cca051c114ac41ad32",J="pageAlignment",K="near",L="fill",M="fillType",N="solid",O="color",P=0xFFFFFFFF,Q="image",R="imageHorizontalAlignment",S="imageVerticalAlignment",T="imageRepeat",U="auto",V="favicon",W="sketchFactor",X="0",Y="colorStyle",Z="appliedColor",ba="fontName",bb="Applied Font",bc="borderWidth",bd="adaptiveStyles",be="interactionMap",bf="diagram",bg="objects",bh="masters",bi="objectPaths";
+return _creator();
+})());
